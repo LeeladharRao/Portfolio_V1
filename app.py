ca27e4410960e3c63ba2cbd1ebdf693d1c48d0a1
Leeladhar Rao <kompallyleeladhar@gmail.com>
2022-04-17 19:50:29 +0530

    Adding blog data

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template, url_for
+import json
+
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    i = 0
+    extra = ()
+    with open ('templates/posts/postsInfo.json', 'r') as f:
+        data = json.loads(f.read())
+    for post in data['post_data']:
+        if (i>=1): break
+        else : extra+= (post,)
+        i+=1
+    return render_template('home.html', postsinfo=extra)
 
 @app.route("/about")
 def about():
@@ -15,7 +26,14 @@ def portfolio():
 
 @app.route("/blog")
 def blog():
-    return render_template('blog.html')
+    with open ('templates/posts/postsInfo.json', 'r') as f:
+        data = json.loads(f.read())
+
+    return render_template('blog.html',postsinfo=data)
+
+@app.route("/blog/<bname>")
+def blogpost(bname):
+    return render_template('posts/'+bname+'.html', url=bname)
 
 if __name__ == '__main__':
     app.run(debug=True)
